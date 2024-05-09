1a4448346e0ce8f4f2e39e5fa3359a0db08b3d15
artur <artur.stepniak@avanade.com>
2024-05-09 10:31:33 +0200

    test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+from Crypto.Cipher import DES, AES
 
 API_KEY = ""
 #test()
@@ -8,3 +9,9 @@ def test():
     #print(API_KEY)
     #if test
     requests.get("https://www.google.com")
+
+
+
+cipher = DES.new(API_KEY)
+def send_encrypted(channel, message):
+    channel.send(cipher.encrypt(message)) # BAD: weak encryption
